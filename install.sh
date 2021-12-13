8ef01c881949d49353afd83928654ea421881c16
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2021-12-13 13:26:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,9 @@ echo "upgrading stuff"
 
 sudo pacman -Syu
 
-sudo pacman -S xfce4 xorg-server xorg-drivers lightdm lightdm-gtk-greeter
+sudo pacman -S xfce4 xorg-server xorg-drivers lightdm lightdm-gtk-greeter inter-font noto-fonts xfce4-power-manager network-manager-applet pulseaudio gvfs
 
-yay -S xfce4-zorinmenulite-plugin
-
-cd ..
+yay -S xfce4-zorinmenulite-plugin xfce4-docklike-plugin
 
 cd zorin-icon-theme/
 makepkg -si
