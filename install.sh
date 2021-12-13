e6159e0a0cd6282fd84999616dc0d15d07a6fa27
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2021-12-13 14:31:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ echo "upgrading stuff"
 
 sudo pacman -Syu
 
+git clone https://aur.archlinux.org/yay
+cd yay/
+makepkg -si
+
+cd ..
+
 sudo pacman -S xfce4 xorg-server xorg-drivers lightdm lightdm-gtk-greeter inter-font noto-fonts xfce4-power-manager network-manager-applet pulseaudio gvfs xfce4-pulseaudio-plugin
 
 yay -S xfce4-zorinmenulite-plugin xfce4-docklike-plugin
