93e93ceff1fd78852acc141123c1ef0306d7cfd2
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2021-12-13 13:16:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-HOMEDIR=${pwd}
-
 echo "upgrading stuff"
 
 sudo pacman -Syu
@@ -10,22 +8,23 @@ sudo pacman -S xfce4 xorg-server xorg-drivers lightdm lightdm-gtk-greeter
 
 yay -S xfce4-zorinmenulite-plugin
 
-cd ${HOMEDIR}
+cd ..
 
 cd zorin-icon-theme/
 makepkg -si
 
-cd ${HOMEDIR}
+cd ..
 
 cd zorin-desktop-themes/
 makepkg -si
 
-cd ${HOMEDIR}
+cd ..
 
 cd zorin-backgrounds/
 makepkg -si
 
-cd ${HOMEDIR}
+cd ..
+
 cd zorin-os-lite-default-settings/
 makepkg -si
 
